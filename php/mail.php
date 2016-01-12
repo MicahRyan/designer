b2c937f7029f3683bbff400c458e3afa2ebb05f7
MicahRyan <contact@micahryan.com>
2016-01-11 23:29:39 -0600

    Contact From
    Updated script and and php file.

---
+++ php/mail.php
@@ -0,0 +1,28 @@
+<?php
+$yourName = $_POST["yourName"];
+$yourEmail = $_POST["yourEmail"];
+$yourNumber = $_POST["yourNumber"];
+$yourWebsite = $_POST["yourWebsite"];
+$yourBudget = $_POST["yourBudget"];
+$startDate = $_POST["startDate"];
+$yourMessage = $_POST["yourMessage"];
+
+$msg = "
+
+Name: $yourName
+Email: $yourEmail
+Number: $yourNumber
+Website: $yourWebsite
+Budget: $yourBudget
+Start Date: $startDate
+Message:
+$yourMessage
+";
+
+$to = "contact@micahryan.com";
+$subject = "Web Design Contact Form";
+$message = $msg;
+$headers = "Web Design Contact Form";
+mail($to,$subject,$message,$headers);
+
+?>
